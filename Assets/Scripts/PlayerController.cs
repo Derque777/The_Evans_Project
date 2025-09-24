a49a78b093fc5a3791a6f71448442e55a02f2adc
Derque777 <210788962+Derque777@users.noreply.github.com>
2025-09-23 21:54:39 -0500

    Inicio the evans proget
    Se comenzo con la programacion

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class WalkController : MonoBehaviour
+{
+    [SerializeField] private float characterController;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float minVelocity;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float scaleView;
+
+    public void Start()
+    {
+
+    }
+
+    public void walk()
+    {
+
+    }
+}
